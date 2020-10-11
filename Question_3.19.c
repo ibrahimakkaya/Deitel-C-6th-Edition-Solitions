8eadd20c2c0caabec1bf0757f8d8854a3af0cb16
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-10-11 23:12:21 +0300

    Add files via upload

---
+++ Question_3.19.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int baz,satis,maas;
+	baz=200;
+	
+		while(satis!=-1)
+		{
+		printf("Satis tutarinizi giriniz: ");
+		scanf("%d", &satis);
+		maas=((baz+satis)/100*9)+baz;
+		printf("Total maasiniz: %d\n",maas);
+			
+		}
+		
+				
+	
+	
+	
+	
+	
+	
+	
+	
+}
