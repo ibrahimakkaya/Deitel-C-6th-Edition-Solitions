b6dab6057565d71195ce933a5764225ae3f65fb3
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-10-14 23:52:43 +0300

    Add files via upload

---
+++ Question_3.28.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int z,a;
+	z=1;
+		while(z=1){
+		
+			printf("Sayi giriniz: ");
+			scanf("%d", &a);
+				if(a==1 || a==2)
+				{
+					printf("Dongu bitti.");
+					break;
+					
+				}
+		}
+	
+}
