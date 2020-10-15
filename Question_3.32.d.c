c792046732b32e8263a086f5026bfa2f650ddf6e
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-10-15 22:46:51 +0300

    Add files via upload

---
+++ Question_3.32.d.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 5, y = 7;
+		
+		if(y==8)
+		{
+		}
+		if(x==4)
+			printf("@@@@@\n");
+		else
+			printf("#####\n");
+			printf("$$$$$\n");
+			printf("&&&&&&\n");
+	
+}
