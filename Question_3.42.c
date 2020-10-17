2a76d907830439763428923e9abf2843b1c9056a
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-10-17 23:57:49 +0300

    Add files via upload

---
+++ Question_3.42.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	float alan,cevre,r,pi = 3.14159;
+	printf("Yaricapi giriniz:");
+	scanf("%f", &r);
+	cevre=2*pi*r;
+	alan=pi*r*r;
+	printf("Alan: %f\n", alan);
+	printf("Cevre: %f", cevre);
+	
+	
+}
