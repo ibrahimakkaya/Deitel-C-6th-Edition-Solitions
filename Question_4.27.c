7aff748a3d10c389c3b1951d1e05c55d7be5854c
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-03 23:29:17 +0300

    Add files via upload

---
+++ Question_4.27.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+int main()
+{
+    int x, y, z;
+
+    for(x=1;x<=500;x++){
+        for(y=1;y<=500;y++){
+            for(z=1;z<=500;z++){
+                if(pow(x,2) == pow(y,2)+pow(z,2))
+                    printf("%d %d %d\n", y,z,x);
+            }
+        }
+    }
+    getch();
+    return 0;
+}
