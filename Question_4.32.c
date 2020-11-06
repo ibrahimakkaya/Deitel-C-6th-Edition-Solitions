297fbcf81d63b404bcd0e7e52abfdd59224a5fcf
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-06 23:55:01 +0300

    Add files via upload

---
+++ Question_4.32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int x, y, z, t, u;
+    float s;
+    printf("1-19 arasi bir tek tamsayi giriniz : ");
+    scanf("&d", &x);
+
+
+    for(y=1;y<=x/2+1;y++){
+            printf("*");
+
+    }
+
+
+
+
+    getch();
+    return 0;
+}
