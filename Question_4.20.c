6b61ba0092709788a4249658794be435650faba9
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-12 18:17:40 +0300

    Create Question_4.20.c

---
+++ Question_4.20.c
@@ -0,0 +1,20 @@
+Aşağıdaki boşlukları 0 veya 1 ile doldurunuz.
+
+
+Koşul1                          Koşul2                          Koşul1 && Koşul2
+  0                               0                                     0
+  0                               0 olmayan (1)                         0
+  0 olmayan                       0                                 ____0_____
+  0 olmayan                       0 olmayan                         ____1_____
+  
+  
+Koşul1                          Koşul2                          Koşul1 || Koşul2
+  0                               0                                     0
+  0                               0 olmayan                             1  
+  0 olmayan                       0                                 ____1_____
+  0 olmayan                       0 olmayan                         ____1_____
+  
+  
+Koşul1                          !Koşul1
+  0                               1
+  0 olmayan                    ___0____
