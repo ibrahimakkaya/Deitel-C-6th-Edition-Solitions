db646b502006fa603f689cfab6a04f11c148bf54
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-12 18:27:48 +0300

    Add files via upload

---
+++ Question_4.21.c
@@ -0,0 +1,18 @@
+// Sekil 4.2'deki programi, syac degiskeninin ilk degeri for yapisinda degil de, degiskenin
+//bildirilmesi sirasindsa verilecegi sekilde tekrar yaziniz.
+
+
+#include <stdio.h>
+
+int main(){
+	
+	int sayici;
+	sayici = 1;
+	
+	for(sayici ; sayici <= 10 ; sayici++)
+		printf("%d\n", sayici);
+	
+	
+	
+	return 0;
+}
