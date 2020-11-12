b10272ac973372c2b43acb7701cceef5b4b18ec2
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-12 19:34:48 +0300

    Add files via upload

---
+++ Question_4.24.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	
+	int i = 1 , j = 2 , k = 3 , m = 2;
+	
+	// printf("%d\n" , i == 1);
+	// printf("%d\n", i >=1 && j < 4); // Dogrulugunu kontrol edip buna gore 0 veya 1 deger donduruyor.
+	// printf("%d", j>= i || k == m);
+	printf("%d", !m);
+}
