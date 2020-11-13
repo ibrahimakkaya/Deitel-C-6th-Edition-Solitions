ce24ae97f168e8e79874e2dc571b5cfb28308024
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-13 23:24:53 +0300

    Update Question_4.32.c

--- Question_4.32.c
+++ Question_4.32.c
@@ -1,22 +1,58 @@
-#include <stdio.h>
-#include <conio.h>
-
-int main()
-{
-    int x, y, z, t, u;
-    float s;
-    printf("1-19 arasi bir tek tamsayi giriniz : ");
-    scanf("&d", &x);
+# include <stdio.h>
 
 
-    for(y=1;y<=x/2+1;y++){
-            printf("*");
-
-    }
-
-
-
-
-    getch();
-    return 0;
+int main(){
+	
+	int satir,yildiz,bosluk,maxsatir;
+	printf("kac satirlik bir yildiz istediginizi giriniz.");
+	scanf("%d",  &maxsatir);
+	
+	for(satir = 1 ; satir <= maxsatir/2 + 1 ; satir++){
+		
+		for(bosluk = 1 ; bosluk <= maxsatir/2 + 1-satir  ; bosluk++){
+			
+			printf(" ");
+		}	
+	
+		for(yildiz = 0 ; yildiz < 2*satir-1 ; yildiz++ ){
+			
+			printf("*");
+		
+		}
+		
+		printf("\n");
+		
+		
+		
+		
+	}
+	
+	for(satir = 1 ; satir <= maxsatir / 2  ; satir++)
+	{
+		for(bosluk = 1 ; bosluk <=satir ; bosluk++)
+		{
+			printf(" ");
+		}
+		
+		for(yildiz = 1 ; yildiz <= maxsatir - 2*satir ; yildiz++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
