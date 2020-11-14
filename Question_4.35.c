d1c91623edd4eb7eb0afd8fdad81996ecb23a6fa
Halil Ibrahim Akkaya <halil.ibrahim.akkaya@icloud.com>
2020-11-14 19:36:25 +0300

    Add files via upload

--- Question_4.35.c
+++ Question_4.35.c
@@ -1,33 +1,106 @@
-4.35 do/while döngüsünü, while döngüsüne dönüştürebilmek için gereken işlemleri anlatınız. Bir while döngüsünü,
-do/while döngüsü ile değiştirmeye çalıştığımızda ne gibi bir problemle
-142
-karşılaşırız. Eğer size bir while döngüsünü, do/while ile değiştirmeniz söylenirse hangi kontrol yapısını eklemeye ihtiyaç duyarsınız ve 
-bu test yapısını programın sonucunun değişmediğini anlayacak şekilde nasıl kullanırsınız?
+#include <stdio.h>
 
-- Do-while döngüsü, koşulun her zaman döngünün gövdesinden sonra yürütülmesi dışında while döngüsü ile hemen hemen aynıdır.
-Bir while döngüsünde, gövde yalnızca belirli bir koşul karşılandığında çalıştırılır ve koşul yanlış olduğunda sona erer. 
-Bu ilk denemede veya yirmi beşinci denemede olabilir. Diğer yandan do-while döngüsü, ilk deneme sırasında koşulun yanlış olup olmadığına bakılmaksızın,
-gövdenin her zaman en az bir kez yürütülmesini garanti eder. While döngüsünün aksine, döngüyü durdurma koşulu döngüdeki ifadeler çalıştırılıncaya kadar test edilmez.
+// Ornek koddur.
 
-while (kosul)
+int getFirstDayOfYear( int );
+int printMonth( int, int, char* );
+void printYear( int );
 
-{     statement 1;
 
-      statement 2;
+int main()
+{   
+    int year;
+    printf("Enter a year from 1994 upwards: ");
+    scanf("%d", &year);
 
-      .
+    printYear(year);
+    
+    return 0;
+}
 
-      .
 
-      statement(n);
+int getFirstDayOfYear(int year) 
+{
+    int currentDay=7;
 
-  }
-  
-  
-do {
+    while(year>1994) 
+    {
+        if(year==1997 || ((year-1-1996)%4==0) ) {
+            currentDay+=366;
+        }
+        else
+            currentDay+=365;
+        
+        year--;
+    }
+    
+    currentDay = currentDay % 7;
+    if(currentDay == 0)
+        currentDay = 7;
+    
+    return currentDay;
+}
 
-       .
 
-       statements // body of loop;
 
-       } while (kosul);
+int printMonth(int startDay, int daysInMonth, char* month) 
+{
+    int i;
+    int currentDay=startDay;
+    
+    printf("---------------------------------------------------\n");
+    printf("\t\t%s\n", month);   
+    printf("---------------------------------------------------\n");
+    
+    printf("SUN\tMON\tTUE\tWED\tTHU\tFRI\tSAT\n\n");
+    
+    for(i=1;i<startDay;i++) {
+        printf("\t");
+    }
+    
+    for(i=1; i<=daysInMonth; i++)
+    {
+        printf("%d\t",i);
+        if(currentDay%7==0)
+            printf("\n");
+        currentDay++;
+    }
+    
+    printf("\n\n"); 
+    
+    currentDay = currentDay % 7;
+    if(currentDay == 0)
+        currentDay = 7;
+    
+    return currentDay;
+}
+
+
+void printYear(int year)
+{
+    int currentDay = getFirstDayOfYear(year); 
+
+    printf("---------------------------------------------------\n");
+    printf("\tCALENDER FOR THE YEAR %d\n", year);   
+    printf("---------------------------------------------------\n");
+
+    currentDay = printMonth(currentDay, 31, "JANUARY");
+    
+    if(year==1996 || ((year-1996)%4==0) ) {
+        printf("%d is a leap year\n", year);
+        currentDay = printMonth(currentDay, 29, "FEBRUARY");
+    }
+    else
+        currentDay = printMonth(currentDay, 28, "FEBRUARY");
+        
+    currentDay = printMonth(currentDay, 31, "MARCH");
+    currentDay = printMonth(currentDay, 30, "APRIL");
+    currentDay = printMonth(currentDay, 31, "MAY");
+    currentDay = printMonth(currentDay, 30, "JUNE");
+    currentDay = printMonth(currentDay, 31, "JULY");
+    currentDay = printMonth(currentDay, 31, "AUGUST");
+    currentDay = printMonth(currentDay, 30, "SEPTEMBER");
+    currentDay = printMonth(currentDay, 31, "OCTOBER");
+    currentDay = printMonth(currentDay, 30, "NOVEMBER");
+    currentDay = printMonth(currentDay, 31, "DECEMBER");
+}
